fb8d987d7694e39c53a61cd19b7fad1d4eae2815
Olivier Bouly <olivierbouly13@gmail.com>
2023-07-15 00:56:25 -0400

    made the basic outline for the chat

--- lua/autorun/client/cl_chat.lua
+++ lua/autorun/client/cl_chat.lua
@@ -14,8 +14,6 @@ sChat.channels = {"global", "local", "dm", "admin", "trade"}
 
 sChat.chatMessages = {}
 
-sChat.inChatMode = false
-
 sChat.Color = {
 	globalChat = Color(91, 233, 115),
 	localChat = Color(214, 164, 56),
@@ -25,17 +23,13 @@ sChat.Color = {
 	recruitmentChat = Color(255, 252, 80)
 }
 
-sChat.config = {
-	timeStamps = false,
-	position = 1,	
-	fadeTime = 12,
-}
+local gradient = Material("gui/center_gradient")
+local gradient2 = Material("vgui/gradient_up")
+local gradient3 = Material("vgui/gradient_down")
 
-sChat.lastMessage = nil
+local hasOpenedPanel = false
 
-sChat.temp = {
-	lastChatType = "global"
-}
+local chatType = "global"
 
 surface.CreateFont( "sChat_18", {
 	font = "Roboto Lt",
@@ -64,28 +58,109 @@ function sendToServer(msgType, text, channel, target)
 		net.WriteString(channel)
 		net.WriteString(target)
 	net.SendToServer()
-
 end
+
+if not GAMEMODE then
+	hook.Remove("Initialize", "sChat_init")
+	hook.Add("Initialize", "sChat_init", function()
+		include("autorun/client/cl_chat.lua")
+	end)
+	return
+end
+
+local function IsPlayerInPropMenu()
+    return IsValid(g_SpawnMenu) and g_SpawnMenu:IsVisible()
+end
+
 
 local function ChatBoxPanel()
 
     local frame = vgui.Create( "DFrame" )
-    frame:SetSize(ScrW() * 0.18, ScrH() * 0.25)
+    frame:SetSize(ScrW() * 0.25, ScrH() * 0.4)
     frame:SetTitle("")
     frame:ShowCloseButton(true) //temp
     frame:SetVisible(true)
-    frame:SetPos(ScrW() * 0.05, ScrH() * 0.4)
-    frame:UseDown = true
+    frame:SetPos(ScrW() * 0.005, ScrH() * 0.5)
+    frame.UseDown = true
     frame:SetDraggable(false)
     frame:SetAlpha(0)
-    frame:AlphaTo(255, 0.3)
+    frame:AlphaTo(255, 0.1)
     frame:MakePopup()
     local DoClose = false 
 
+    if not frame then return end
+
+    function frame:Paint( w, h )
+
+        surface.SetDrawColor( 20,20,20, 40)
+        surface.DrawRect( 0, 0, w, h )
+
+        surface.SetDrawColor( 219,219,219,178)       
+        surface.DrawOutlinedRect( 0, 0, w, h )
+
+    end
+
+    local chatBarPanel = vgui.Create("Panel", frame)
+    chatBarPanel:SetSize(frame:GetWide(), 50)
+    chatBarPanel:SetPos(0, frame:GetTall() - 50)
+    function chatBarPanel:Paint( w, h )
+
+        surface.SetDrawColor(20,20,20, 10)
+        surface.DrawRect(0, 0, w, h )
+
+        surface.SetDrawColor( 219,219,219,178)       
+        surface.DrawOutlinedRect( 0, 0, w, h )
+
+    end
+
+    function frame:Think()
+        if IsPlayerInPropMenu() then return end
+    
+        if self.UseDown and not input.IsKeyDown(KEY_Y) then
+            self.UseDown = false
+            return
+        elseif not self.UseDown and input.IsKeyDown(KEY_ESCAPE) then
+            gui.HideGameUI()
+            frame:SetAlpha(255)
+            frame:AlphaTo(0, 0.1)
+            timer.Create("frameFaded", 0.2, 0, function()
+                self:Close()
+                hasOpenedPanel = false
+                timer.Remove("frameFaded")
+            end)
+        end
+    end
 end
+
+timer.Create("ChatBoxPanel", 0, 0, function()
+    if IsPlayerInPropMenu() or LocalPlayer():IsTyping() then return end
+    if input.IsKeyDown(KEY_Y) and not hasOpenedPanel then
+        ChatBoxPanel()
+        hasOpenedPanel = true
+        DoClose = false
+    end
+end)
 
 concommand.Add("test_ui", function()
 
-    sChat.frame:Paint(200, 200)
+    ChatBoxPanel()
 
 end)
+
+--// Hide the default chat too in case that pops up
+hook.Remove("HUDShouldDraw", "sChat_hidedefault")
+hook.Add("HUDShouldDraw", "sChat_hidedefault", function( name )
+	if name == "CHudChat" or name == "CHudHealth" then
+		return false
+	end
+end)
+
+hook.Add( "PlayerBindPress", "overrideChatbind", function( ply, bind, pressed )
+    local bTeam = false
+    if bind == "messagemode" then
+    elseif bind == "messagemode2" then
+    else
+        return
+    end
+    return true
+end )
